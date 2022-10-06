59ec6a62db2eba0418f957941cf9f1c474e43df8
Eslam <islam2520@gmail.com>
2022-10-06 19:09:01 +0200

    adding models

---
+++ src/main/java/com/drone/api/model/DroneDTO.java
@@ -0,0 +1,36 @@
+package com.drone.api.model;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+public class DroneDTO {
+
+    @NotNull
+    @Size(max = 100)
+    private String serialNumber;
+
+    @NotNull
+    private String model;
+
+    @NotNull
+    @Max(value = 500)
+    private Double weightLimit;
+
+    @NotNull
+    private Double batteryCapacity;
+
+    @NotNull
+    private String state;
+
+    private List<MedicationDTO> medications;
+
+}
---
+++ src/main/java/com/drone/api/model/MedicationDTO.java
@@ -0,0 +1,26 @@
+package com.drone.api.model;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class MedicationDTO {
+
+    @NotNull
+    @Pattern(regexp = "^[A-Z0-9_]*$")
+    private String code;
+
+    @NotNull
+    @Pattern(regexp = "^[\\w-]+$")
+    private String name;
+
+    @NotNull
+    private Double weight;
+
+    private String image;
+
+}
