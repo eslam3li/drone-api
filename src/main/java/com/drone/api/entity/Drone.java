b2cd48705590360ca6c9963e294f9e6e1d5cdedf
Eslam <islam2520@gmail.com>
2022-10-06 18:36:13 +0200

    adding required pom dependencies and creating the entities

---
+++ src/main/java/com/drone/api/entity/Drone.java
@@ -0,0 +1,39 @@
+package com.drone.api.entity;
+
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Drone {
+
+    @Id
+    @Column(length = 100)
+    private String serialNumber;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(nullable = false)
+    private Double weightLimit;
+
+    @Column(nullable = false)
+    private Double batteryCapacity;
+
+    @Column(nullable = false)
+    private String state;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "drone_medications",
+            joinColumns = @JoinColumn(name = "drone_serial_number"),
+            inverseJoinColumns = @JoinColumn(name = "medication_code")
+    )
+    private List<Medication> medications;
+
+}
---
+++ src/main/java/com/drone/api/entity/Medication.java
@@ -0,0 +1,31 @@
+package com.drone.api.entity;
+
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Medication {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double weight;
+
+    @Column
+    private String image;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL, mappedBy = "medications")
+    private List<Drone> drones;
+
+}
