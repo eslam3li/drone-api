ea3de8af372e2d18c33c176868b0e314c807cf18
Eslam <islam2520@gmail.com>
2022-10-08 11:36:46 +0200

    periodic task to check drones battery levels

---
+++ src/main/java/com/drone/api/util/BatteryLevelChecker.java
@@ -0,0 +1,40 @@
+package com.drone.api.util;
+
+import com.drone.api.model.DroneDTO;
+import com.drone.api.service.DroneServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.List;
+
+@Slf4j
+@Configuration
+@EnableScheduling
+public class BatteryLevelChecker {
+
+    private DroneServiceImpl droneService;
+
+    BatteryLevelChecker(DroneServiceImpl droneService) {
+        this.droneService = droneService;
+    }
+
+    @Scheduled(cron = "00 03 * * * ?")
+    public void batteryLevelChecker() {
+        log.info("Checking all drones battery level");
+        List<DroneDTO> drones = droneService.getAllDrones();
+
+        if (drones != null) {
+            for (DroneDTO drone : drones) {
+                log.info("{} drone with serial number [{}], has {}% battery capacity",
+                        drone.getModel(),
+                        drone.getSerialNumber(),
+                        drone.getBatteryCapacity() * 100);
+            }
+        } else
+            log.info("No Drone found!");
+
+        log.info("End of battery check");
+    }
+}
